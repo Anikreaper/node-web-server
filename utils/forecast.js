6f72da3bca443ac954c99c35eef407b04921da17
Anik Mondal <anik.mondal@codeclouds.in>
2021-12-31 16:26:53 +0530

    improvement

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,19 +1,21 @@
 const request = require('postman-request');
 
 const forecast = (data, callback) => {
-    const url = "http://api.weatherstack.com/current?access_key=eb219b340d00a75dc2d54d52fa644bfe&query="+data.lat+','+data.lon;
+    const url = "http://api.weatherstack.com/current?access_key=eb219b340d00a75dc2d54d52fa644bfe&query=" + data.lat + ',' + data.lon;
 
-    request({ url: url, json: true }, (err, res) => {
+    request({
+        url: url,
+        json: true
+    }, (err, res) => {
         if (err) {
             callback('Unable to location service!', undefined)
-        }
-        else if (res.body.length == 0) {
+        } else if (res.body.length == 0) {
             callback('Unable to find location. Please try another search!', undefined)
+        } else {
+
+            callback(undefined, res.body);
         }
-        else {
-            callback(undefined,res.body);
-        }
-        
+
 
 
     })
