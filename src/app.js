314aba24cf2d3ef77c50dcd9e1a9f424dffc2ecf
Anik <anikversatile64@gmail.com>
2022-01-07 00:15:02 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -87,6 +87,6 @@ app.get("*", (req, res) => {
 
 
 
-app.listen(port, () => {
-    console.log("Server is running on port ")
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
